775a80b6ec7cf0c2330d13356028eb3705e3276a
Dennis Yiaile <ykamakei6345@gmail.com>
2020-09-23 18:11:08 +0300

    change padding and margin layout

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,16 @@
-function content(){
-  var elemnt=document.getElementById('content');
-  
+$("document").ready(function(){
+  $("#button").click(function(){
+  var name=$("#name").val();
+  var email=$("#email").val();
+  var message=$("#message").val();
+  if (!name && !email) {
+  alert("Enter your contact details!")
+}else if (!email) {
+  alert(name +", enter your email!");
+} else if (!name) {
+  alert("Enter your name!")
+} else {
+    alert(name+ ", I have received your messsage. I will get back to you in a few!");
 }
+  });
+})
